c6694658e9c4741814b1258200cb6d5992ae25ae
Nguyễn Bách <nxb17111999@gmail.com>
2025-04-25 10:51:52 +0700

    Initial Commit

---
+++ CommandProject/CommandProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommandProjectApp.swift
+//  CommandProject
+//
+//  Created by Nguyễn Bách on 25/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CommandProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
